c605a49c19e19356a126c60a89ff792a8e9f9311
Nikhil <nikhilkudupudi@gmail.com>
2024-10-31 13:49:29 -0400

    Update publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/NikhilKudupudi.io/'  // Replace with your GitHub repo name
+    : '/'
 })
