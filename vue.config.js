3edf06ee09986cae77901a0a8571992a06d35cf0
Nikhil Kudupudi <46317218+Nikhil-Kudupudi@users.noreply.github.com>
2024-10-31 17:42:38 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    publicPath: '/NikhilKudupudi.io'
+    publicPath: '/NikhilKudupudi.io/'
 })
